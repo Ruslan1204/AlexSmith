28dd1f3a67718d7675c6133b2ba3bc3412c1c646
Ruslan <113464399+Ruslan1204@users.noreply.github.com>
2023-09-26 19:25:12 +0300

    s

---
+++ js/swiper.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.portfolio__slider', {
+  // Optional parameters
+
+  loop: true,
+  slidesPerView: 3,
+  // grid: {
+  //   rows: 3,
+  // },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.portfolio__arrow_left',
+    prevEl: '.portfolio__arrow_right',
+  },
+});
+
+// loop="true"
+// observerPerents="true"
+// slidesPerView="3"
+// speceBetween="56"
+// speed="800"
