6834804e17b2eef8d2cb666ffc24365b5044bfa5
Ruslan <113464399+Ruslan1204@users.noreply.github.com>
2023-09-28 16:51:34 +0300

    s

--- js/swiper.js
+++ js/swiper.js
@@ -2,15 +2,35 @@ const swiper = new Swiper('.portfolio__slider', {
   // Optional parameters
 
   loop: true,
-  slidesPerView: 3,
-  // grid: {
-  //   rows: 3,
-  // },
+  // slidesPerView: 3,
+  spaceBetween: 42,
 
-  // Navigation arrows
   navigation: {
     nextEl: '.portfolio__arrow_left',
     prevEl: '.portfolio__arrow_right',
+  },
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      autoHeight: true,
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    // when window width is >= 640px
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 26,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 42,
+    },
   },
 });
 
